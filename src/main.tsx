09f37f6bced0667552ba2aff7593c1aef14b6596
Mohammedmjk <153817981+Mohammedmjk@users.noreply.github.com>
2026-09-25 20:40:44 +0300

    Initialize Sentry for error tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://a7c96a81388ef064498da248c89269b5@o4512148160643072.ingest.de.sentry.io/4512148175126608"
+});
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
